ab9fea89372dee65c14418c3467bdbd198a93ea2
Sic-Mundus-1804 <adarsh1804.nsut@gmail.com>
2021-05-27 12:19:09 +0530

    first commit

---
+++ Nodejs_rest/routes/projects.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+import { getProjects, createProject, getProject, deleteProject} from '../controllers/projects.js';
+
+const router = express.Router();
+
+router.get('/', getProjects);
+
+router.post('/', createProject);
+
+router.get('/:id', getProject);
+
+router.delete('/:id', deleteProject);
+
+export default router;
