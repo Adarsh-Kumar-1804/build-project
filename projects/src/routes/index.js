663d12d817b54b098e6f97354ff1904c1c94bc48
Sic-Mundus-1804 <adarsh1804.nsut@gmail.com>
2021-06-02 16:49:42 +0530

    Used Prettier

--- projects/src/routes/index.js
+++ projects/src/routes/index.js
@@ -1,14 +1,20 @@
 import express from 'express';
 
-import { getProjects, createProject, getProject, deleteProject,getFromCache} from '../services/projectservice.js';
+import {
+  getProjects,
+  createProject,
+  getProject,
+  deleteProject,
+  getFromCache,
+} from '../services/projectservice.js';
 
 const router = express.Router();
 
-router.get('/',getFromCache,getProjects);
+router.get('/', getFromCache, getProjects);
 
 router.post('/', createProject);
 
-router.get('/:id',getFromCache,getProject);
+router.get('/:id', getFromCache, getProject);
 
 router.delete('/:id', deleteProject);
 
--- projects/src/services/projectservice.js
+++ projects/src/services/projectservice.js
@@ -1,69 +1,63 @@
 import { v4 as uuid } from 'uuid';
-import { createRequire } from "module"; 
+import { createRequire } from 'module';
+import NodeCache from 'node-cache';
+
 const require = createRequire(import.meta.url);
-const data = require("../data/data.json");
-import nodeCache from "node-cache";
+const data = require('../data/data.json');
 
-const myCache=new nodeCache({stdTTL:10});
- 
-let projects=data;
+const myCache = new NodeCache({ stdTTL: 10 });
 
-export const getFromCache=(req,res,next)=>{
-      
-    var key="projects"+req.route.path;
-      req.customurl=key;
+const projects = data;
 
-      if(myCache.has(key))
-      {  console.log(`Retrieving data from Cache key: ${key}`);
-          return res.send(myCache.get(key));
-      }
-      else
-      next();
-    }  
+export const getFromCache = (req, res, next) => {
+  const key = `projects${req.route.path}`;
+  req.customurl = key;
+
+  if (myCache.has(key)) {
+    console.log(`Retrieving data from Cache key: ${key}`);
+    return res.send(myCache.get(key));
+  }
+  next();
+};
 
 export const getProjects = (req, res) => {
-    
-    myCache.set(req.customurl,projects);
-    res.send(projects);
-}
+  myCache.set(req.customurl, projects);
+  res.send(projects);
+};
 
-export const createProject = (req, res) => {   
-    const project = req.body;
+export const createProject = (req, res) => {
+  const project = req.body;
 
-    projects.push({id: uuid(),...project});
-    
-    console.log(`Project [${project.name}] added to the database.`);
+  projects.push({ id: uuid(), ...project });
 
-    res.send("Project added successfully");
+  console.log(`Project [${project.name}] added to the database.`);
+
+  res.send('Project added successfully');
 };
 
 export const getProject = (req, res) => {
-    var id= req.params.id;
-    
-    var project=projects.find((user)=>user.id===id);
-    
-    if(!project)
-    {
-        res.send("The project does not exist");
-    }
-    
-    myCache.set(req.customurl,project);
-    res.send(project);
+  const { id } = req.params;
 
+  const project = projects.find((user) => user.id === id);
+
+  if (!project) {
+    res.send('The project does not exist');
+  }
+
+  myCache.set(req.customurl, project);
+  res.send(project);
 };
 
 export const deleteProject = (req, res) => {
-      
-    var id=req.params.id;
-    
-    console.log(`Project with id ${id} has been deleted`);
-     
-    projects.forEach((user,index)=>{
-       if(req.params.id===user.id)
-       {
-           projects.splice(index,1);
-       }
-    });
+  const { id } = req.params;
 
-    res.send(`Project with id ${id} has been deleted`);
+  console.log(`Project with id ${id} has been deleted`);
+
+  projects.forEach((user, index) => {
+    if (req.params.id === user.id) {
+      projects.splice(index, 1);
+    }
+  });
+
+  res.send(`Project with id ${id} has been deleted`);
 };
